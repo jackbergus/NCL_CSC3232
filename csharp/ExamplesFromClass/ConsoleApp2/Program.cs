d36b4b2229224e936346261f3203ad30722453a1
Giacomo Bergami <bergamigiacomo@gmail.com>
2022-11-29 19:57:41 +0000

    Updates to the RL example, so to model the same realistic scenario from MinMax

--- csharp/ExamplesFromClass/ConsoleApp2/Program.cs
+++ csharp/ExamplesFromClass/ConsoleApp2/Program.cs
@@ -6,13 +6,15 @@ namespace ConsoleApp2
     class Program {
         static void Main(string[] args) {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            BasicDungeonGeneration.testing();
+            VariationOnMinMaxRealisticWithRL.main();
+            // Console.OutputEncoding = System.Text.Encoding.UTF8;
+            // BasicDungeonGeneration.testing();
             //probabilities.Probabilities.main();
             ///timing.Timing.main();
             ///goap.GOAP.main();
 
-            Console.WriteLine("Press any key (except Enter) to exit");
-            Console.ReadKey();
+            // Console.WriteLine("Press any key (except Enter) to exit");
+            // Console.ReadKey();
         }
     }
 }
